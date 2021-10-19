6c86519c0665e8b80bc6c0345a87c5e04c924f14
Vinicius Leite <87040226+ViniciusLeitedoCarmo@users.noreply.github.com>
2021-10-19 14:29:30 -0300

    Adding input restriction.

--- mainfile.py
+++ mainfile.py
@@ -1,9 +1,11 @@
 print("Let's calculate the factorial of a number!")
 num=int(input("Enter the number you want: "))
+while num<0:
+  num=int(input("Factorial is just for not negative numbers, try again with another one: "))
 if num==0:
   print(1)
 else:
   for k in range(num,0,-1):
     if k>1:
       num*=k-1
-  else:print(num)
+  else:print("Result: ",num)
